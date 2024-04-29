d704e5aec4689d5de47229a78534a14c244bbaf7
aihassan1 <abdosruor57@gmail.com>
2024-04-29 09:13:13 +0300

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,6 +6,6 @@ export default function handleProfileSignup() {
       console.log(`${photo.body} ${username.firstName} ${username.lastName}`);
     })
     .catch(() => {
-      console.error(`Signup system offline`);
+      console.log('Signup system offline');
     });
 }
