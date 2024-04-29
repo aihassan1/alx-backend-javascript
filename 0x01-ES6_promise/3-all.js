a19cea5bbf1bf5b3c944ada62ad1934c2a7f8887
aihassan1 <abdosruor57@gmail.com>
2024-04-29 09:11:16 +0300

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([photo, username]) => {
-      console.log(photo.body, username.firstName, username.lastName);
+      console.log(`${photo.body} ${username.firstName} ${username.lastName}`);
     })
 
     .catch(() => {
