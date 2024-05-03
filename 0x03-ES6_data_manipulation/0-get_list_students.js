08449fe8027321eabd44fd831d646052ba902582
aihassan1 <abdosruor57@gmail.com>
2024-05-03 15:34:05 +0300

    0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,21 +1,19 @@
 export default function getListStudents() {
-  return [
-    {
-      id: 1,
-      firstName: 'Guillaume',
-      location: 'San Francisco',
-    },
-    {
-      id: 2,
-      firstName: 'James',
-      location: 'Columbia',
-    },
-    {
-      id: 3,
-      firstName: 'Serena',
-      location: 'San Francisco',
-    },
-  ];
+  const Guillaume = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+  const James = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+  const Serena = {
+    id: 3,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+  const arrayOfObjects = [Guillaume, James, Serena];
+  return arrayOfObjects;
 }
-
-console.log(getListStudents());
