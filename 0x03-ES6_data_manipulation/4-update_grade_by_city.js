f88f1e6f8ac66940f45566550d314abae077c035
aihassan1 <abdosruor57@gmail.com>
2024-05-03 22:38:27 +0300

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,20 +1,11 @@
 export default function updateStudentGradeByCity(studentList, city, newGrades) {
-  const filterdList = studentList.filter(
-    (student) => student.location === city,
-  );
-
-  // if id newgrade.id ==student_id -> add the grade to the student
-  filterdList.forEach((student) => {
-    student.grade = 'N/A';
-  });
-
-  filterdList.forEach((student) => {
-    for (const studentGrade of newGrades) {
-      if (studentGrade.studentId === student.id) {
-        student.grade = studentGrade.grade;
-      }
-    }
-  });
-
-  return filterdList;
+  return studentList
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+    });
 }
