d554c91130a4dbbe654e283821d206ecebc39f44
aihassan1 <abdosruor57@gmail.com>
2024-06-18 21:10:53 +0300

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,11 @@
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.setEncoding('utf8');
+process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name) {
     process.stdout.write(`Your name is: ${name}`);
   }
 });
-
 process.stdin.on('close', () => {
-  process.stdout.write('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
