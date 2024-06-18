1a34ee5e5c7701ff30743c37b8087c645356c922
aihassan1 <abdosruor57@gmail.com>
2024-06-18 20:57:38 +0300

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,19 +1,12 @@
-const readline = require('readline');
-
-r1 = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-console.log('Welcome to Holberton School, what is your name?');
-
-r1.on('line', (name) => {
-  console.log(`Your name is: ${name}`);
-  r1.close();
-});
-
-r1.on('close', () => {
-  if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
   }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
