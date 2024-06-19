13d31e800b8e1751117730d34d90601e1f4149cd
aihassan1 <abdosruor57@gmail.com>
2024-06-19 10:29:11 +0300

    5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,26 +1,50 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+  try {
+    const content = await fs.readFile(path, 'utf-8');
+
+    const rows = content.split('\n').filter((row) => row.trim() !== '');
+
+    const NumberOfStudents = rows.length - 1;
+
+    rows.shift();
+    const students = rows.map((row) => row.split(','));
+
+    const SWE_SS = [];
+    const CS_SS = [];
+
+    students.forEach((student) => {
+      if (student[3] === 'SWE') SWE_SS.push(` ${student[0]}`);
+      else if (student[3] === 'CS') CS_SS.push(` ${student[0]}`);
+    });
+
+    const output = `Number of students: ${NumberOfStudents}\nNumber of students in CS: ${CS_SS.length}. List:${CS_SS}\nNumber of students in SWE: ${SWE_SS.length}. List:${SWE_SS}`;
+    return output;
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
 
 const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
     const databaseName = process.argv[2];
 
     try {
-      const OriginalConsoleLog = console.log;
-      let output = '';
-      console.log = (msg) => {
-        output += msg + '\n';
-      };
+      let output = 'This is the list of our students';
+      const studentData = await countStudents(databaseName);
 
-      console.log('This is the list of our students');
-      await countStudents(databaseName);
-      console.log = OriginalConsoleLog;
+      output += `${studentData}`;
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end(output);
     } catch (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
       res.end('Cannot load the database');
     }
   }
