2b2cd2cd8de787f138dc2b53eb91172c7e085090
aihassan1 <abdosruor57@gmail.com>
2024-06-22 18:42:24 +0300

    0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,4 +7,7 @@ describe('calculateNumber', function () {
     assert.strictEqual(calculateNumber(4.5, 2.5), 8);
     assert.strictEqual(calculateNumber(-1.4, -2.6), -4);
   });
+  it('should be -2 + 4 = 2', function () {
+    assert.equal(calculateNumber(-2, 4), 2);
+  });
 });
