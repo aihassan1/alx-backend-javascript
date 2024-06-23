38aa88da257609f10b4c5b1ee5c7079c3ff5fcdf
aihassan1 <abdosruor57@gmail.com>
2024-06-23 20:06:23 +0300

    4-payment.

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -7,24 +7,23 @@ const expect = chai.expect;
 
 describe('sendPaymentRequestToApi', () => {
   beforeEach(() => {
-    // create a spy
     sinon.spy(Utils, 'calculateNumber');
   });
 
   it('should be called once', () => {
-    sendPaymentRequestToApi(10, 12);
+    sendPaymentRequestToApi(100, 20);
     expect(Utils.calculateNumber.calledOnce).to.be.true;
   });
 
   it('should be called with 2 args', () => {
-    sendPaymentRequestToApi(10, 20);
-    expect(Utils.calculateNumber.calledWith('SUM', 10, 20)).to.be.true;
+    sendPaymentRequestToApi(100, 20);
+    expect(Utils.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
   });
 
   it('should log the correct sum', () => {
     sinon.spy(console, 'log');
-    sendPaymentRequestToApi(20, 40);
-    expect(console.log.calledWith('The total is: 60')).to.be.true;
+    sendPaymentRequestToApi(100, 20);
+    expect(console.log.calledWith('The total is: 120')).to.be.true;
   });
 
   afterEach(() => {
---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,33 @@
+const sendPaymentRequestToApi = require('./3-payment');
+const sinon = require('sinon');
+const Utils = require('./utils');
+const chai = require('chai');
+
+const expect = chai.expect;
+
+describe('sendPaymentRequestToApi', () => {
+  let calculateNumberStub;
+  beforeEach(() => {
+    calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+  });
+
+  it('should be called once', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(Utils.calculateNumber.calledOnce).to.be.true;
+  });
+
+  it('should be called with 2 args', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(Utils.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+  });
+
+  it('should log the correct sum', () => {
+    sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    expect(console.log.calledWith('The total is: 10')).to.be.true;
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+});
