97fb18d38495622ea7981959ff8fd1d799901bef
aihassan1 <abdosruor57@gmail.com>
2024-06-23 15:12:11 +0300

    2-calcul_chai.

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -2,29 +2,27 @@ const calculateNumber = require('./1-calcul');
 const chai = require('chai');
 const expect = chai.expect;
 
-describe('tests for calculateNumber function', function () {
-  describe('type=SUM', () => {
-    it('should round 2 numbers then add them 2.5 + 1.4 = 4', () => {
+describe('calculateNumber', function () {
+  describe('type=SUM', function () {
+    it('should round 2 numbers then add them 2.5 + 1.4 = 4', function () {
       expect(calculateNumber('SUM', 2.5, 1.4)).to.equal(4);
       expect(calculateNumber('SUM', 2, 1)).to.equal(3);
     });
   });
 
-  describe('type=Subtract', () => {
-    it('should round the two numbers, and subtract b from a 5.5 - 3.4 = 3', () => {
+  describe('type=Subtract', function () {
+    it('should round the two numbers, and subtract b from a 5.5 - 3.4 = 3', function () {
       expect(calculateNumber('SUBTRACT', 5.5, 3.4)).to.equal(3);
       expect(calculateNumber('SUBTRACT', 6, 3)).to.equal(3);
     });
   });
 
-  describe('type=DIVIDE', () => {
-    it(`DIVIDE, round the two numbers, and divide a with b ->
-      6.4 / 2.5 = 2,  5 / 0.3 => Error`, () => {
+  describe('type=DIVIDE', function () {
+    it('DIVIDE, round the two numbers, and divide a with b -> 6.4 / 2.5 = 2,  5 / 0.3 => Error', function () {
       expect(calculateNumber('DIVIDE', 5.5, 2.7)).to.equal(2);
     });
 
-    it(`should, return the string 'Error' if the rounded value of b is equal to 0
-      6.4 / 2.5 = 2,  5 / 0.3 => Error`, () => {
+    it('should, return the string -Error- if the rounded value of b is equal to 0 6.4 / 2.5 = 2,  5 / 0.3 => Error', function () {
       expect(calculateNumber('DIVIDE', 5, 0.3)).to.equal('Error');
     });
   });
