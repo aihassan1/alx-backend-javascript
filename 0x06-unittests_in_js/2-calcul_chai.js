79637f2ca79f0b3d33e43fcaffca3fc76a96ac69
aihassan1 <abdosruor57@gmail.com>
2024-06-23 15:17:55 +0300

    2-calcul_chai.

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -8,8 +8,7 @@ function calculateNumber(type, a, b) {
   if (type === 'DIVIDE') {
     if (Math.round(b) === 0) {
       return 'Error';
-    }
-    return Math.round(a) / Math.round(b);
+    } else return Math.round(a) / Math.round(b);
   }
 }
 module.exports = calculateNumber;
